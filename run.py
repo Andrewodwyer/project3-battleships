48eac324059f7646cd30f3c699a8cc8573ed0c52
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-05-09 11:16:44 +0000

    created a comment with description of idea for gameboard and printed 2 words

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+X = x-axis
+Y = y-axis
+"_" is the gameboard marking/spaces
+Gameboard will be 8 * 8 in size
+"." = water, space available to guess
+“@" = players ship
+"X" = Ship that was hit with bullet
+“O" = Water that was shot with bullet, a miss because it hit no ship
+
+Will need to chabge letters to numbers for column and user .upper() so if the player types lowercase it will be changed to upper 
+Dictionary {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+"""
+
+print("Battleships")
+print("rules")
