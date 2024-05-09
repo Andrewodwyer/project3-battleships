7786f9db9df790bffe60b5784418118fc9b53a5b
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-05-09 13:08:02 +0000

    added rules

--- run.py
+++ run.py
@@ -12,5 +12,7 @@ Will need to chabge letters to numbers for column and user .upper() so if the pl
 Dictionary {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 """
 
-print("Battleships")
-print("rules")
+print("Battleships\n")
+print("Battleships is a classic two-player game played on a grid.\nYou'll play against the computer, each having 5 ships. \nThe first to hit all 5 ships is the winner. \nYou will see 2 grids, first is your grid with ship placement and the second is the computers grid. \nThe computers grid won’t display their ship positions.\nHowever it will be mark with an “X” if you get a hit or “O” if it’s a miss.\nTake a shot by entering coordinates (e.g., A1, B5) on the grid. \nThe goal is to sink all of the opponent's ships before they sink yours.\n")
+
+print()
